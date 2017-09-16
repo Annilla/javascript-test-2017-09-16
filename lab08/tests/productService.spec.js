f0b9e985582050ca9d97b9426f060fe7f6a1214b
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-09-16 15:59:17 +0800

    Lab08

--- lab08/tests/productService.spec.js
+++ lab08/tests/productService.spec.js
@@ -10,6 +10,16 @@ describe('ProductService', () => {
     describe('#getProduct()', () => {
         it('should record visitor count when get product', () => {
             // 練習使用 Mock
+            // Arrange
+            var productService = new ProductService();
+            var mock = sinon.mock(productService);
+            mock.expects('addVisitor').once();
+                
+            // Act
+            actual = productService.getProduct();
+                
+            // Assert
+            mock.verify();
         });
     });
 });
